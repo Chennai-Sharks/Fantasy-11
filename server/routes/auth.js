7cb44e7ebafe3e23ccdb85ab1bd8fa186c2cc93a
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-23 20:50:33 +0530

    Send OTP

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const crypto = require('crypto');
 
 //register auth route
 router.post('/register', async (req, res) => {
@@ -52,8 +53,38 @@ router.post('/login', async (req, res) => {
   //create and assigning the tokens
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
   res.header('auth_Token', token).send(token);
-  res.send({_id: user._id});
 });
+
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid,authToken)
+
+const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN
+const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN
+
+const smsKey = process.env.SMS_SECRET_KEY
+
+app.post('/sendOTP', (req, res) => {
+	const phone = req.body.phone;
+	const otp = Math.floor(100000 + Math.random() * 900000);
+	const ttl = 2 * 60 * 1000;
+	const expires = Date.now() + ttl;
+	const data = `${phone}.${otp}.${expires}`;
+	const hash = crypto.createHmac('sha256', smsKey).update(data).digest('hex');
+	const fullHash = `${hash}.${expires}`;
+
+	client.messages
+		.create({
+			body: `Your One Time Login Password For Fantasy 11 is ${otp}`,
+			from: +18643830688,
+			to: phone
+		})
+		.then((messages) => console.log(messages))
+		.catch((err) => console.error(err));
+
+	res.status(200).send({ phone, hash: fullHash, otp });  // this bypass otp via api only for development instead hitting twilio api all the time
+	//res.status(200).send({ phone, hash: fullHash });          // Use this way in Production
+}); 
 
 
 module.exports = router;
