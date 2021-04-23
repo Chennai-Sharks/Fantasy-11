24f95976d277c7ec29d40bf929244881808af34b
Poujhit <poujhit.dev@gmail.com>
2021-04-23 23:54:03 +0530

    Added different pages

---
+++ website/pages/login/index.tsx
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import React from 'react';
+import { Button, Card, Typography } from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+
+import classes from '../../styles/Login.module.scss';
+import styles from '../../styles/Home.module.scss';
+
+const LoginScreen: React.FC = () => {
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Fantasy 11 | Login</title>
+			</Head>
+			<div className={classes.background}>
+				<Card
+					className={classes.Card}
+					variant='elevation'
+					raised
+					style={{
+						borderRadius: '20px',
+					}}
+				>
+					<div className={classes.leftPortionCard}>
+						<Typography
+							className={styles.title}
+							style={{
+								color: 'white',
+							}}
+						>
+							Login
+						</Typography>
+					</div>
+					<div className={classes.rightPortionCard}>
+						<Button
+							className={classes.Button}
+							startIcon={<FacebookIcon style={{ color: '#4267B2' }} />}
+						>
+							Login With Facebook
+						</Button>
+						<Typography
+							className={styles.title}
+							style={{
+								fontSize: '20px',
+							}}
+						>
+							Or
+						</Typography>
+						<Button className={classes.Button}>Login using OTP</Button>
+						<Typography
+							className={styles.title}
+							style={{
+								fontSize: '20px',
+							}}
+						>
+							Or
+						</Typography>
+						<Button className={classes.Button}>
+							Login Using Email and Passoword
+						</Button>
+					</div>
+				</Card>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default LoginScreen;
