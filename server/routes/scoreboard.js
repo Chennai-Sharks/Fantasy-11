d4924f3a0c96f2b857a5f8cfa718f547138583bb
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-24 16:44:59 +0530

    Minor fixes

--- server/routes/scoreboard.js
+++ server/routes/scoreboard.js
@@ -1,10 +1,7 @@
 const express = require('express').Router();
 
 router.get('/:id', async (req,res) => {
-  const ans = await User.findById(req.params.id);
+  const score = await User.findById(req.params.id);
+  score = score.
   res.send(ans);
-});
-
-router.post('/user/scorecardDetails', async (req,res) => {
-
 });
