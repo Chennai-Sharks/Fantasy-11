8bc3063477d4deec2913df0fe43de842051869ed
Poujhit <poujhit.dev@gmail.com>
2021-04-24 10:00:33 +0530

    Changed primary colour

--- website/theme/Default-theme.ts
+++ website/theme/Default-theme.ts
@@ -6,7 +6,7 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			main: '#556cd6',
+			main: '#fd3a4b',
 		},
 		secondary: {
 			main: '#19857b',
