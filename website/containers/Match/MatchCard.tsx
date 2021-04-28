018b06c5a8ebe3895577667a91aebaf6ef5efb89
Poujhit <poujhit.dev@gmail.com>
2021-04-28 18:27:41 +0530

    Added animation on match screen card

--- website/containers/Match/MatchCard.tsx
+++ website/containers/Match/MatchCard.tsx
@@ -1,8 +1,6 @@
-import { Button, Card, Snackbar, Typography } from '@material-ui/core';
-import axios from 'axios';
 import React from 'react';
-import { useQuery } from 'react-query';
-import { MoonLoader } from 'react-spinners';
+import { Card, Typography } from '@material-ui/core';
+import { motion, Variants } from 'framer-motion';
 
 import classes from './MatchCard.module.scss';
 
@@ -12,14 +10,42 @@ interface MatchCardProps {
 	onTap: () => void;
 }
 
+const fadeInUp: Variants = {
+	initial: {
+		y: 60,
+		opacity: 0,
+		transition: { duration: 0.6, ease: [0.6, -0.05, 0.01, 0.99] },
+	},
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			duration: 0.6,
+			ease: [0.6, -0.05, 0.01, 0.99],
+		},
+	},
+};
+
 const MatchCard: React.FC<MatchCardProps> = (props) => {
 	return (
 		<React.Fragment>
-			<Card raised className={classes.MatchCard} onClick={props.onTap}>
-				<Typography className={classes.subTitle}>{props.one}</Typography>
-				<Typography className={classes.subTitle}>Vs</Typography>
-				<Typography className={classes.subTitle}>{props.two}</Typography>
-			</Card>
+			<motion.div
+				style={{
+					width: '100%',
+
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+				whileHover={{ scale: 1.03 }}
+				variants={fadeInUp}
+				whileTap={{ scale: 0.95 }}
+			>
+				<Card raised className={classes.MatchCard} onClick={props.onTap}>
+					<Typography className={classes.subTitle}>{props.one}</Typography>
+					<Typography className={classes.subTitle}>Vs</Typography>
+					<Typography className={classes.subTitle}>{props.two}</Typography>
+				</Card>
+			</motion.div>
 		</React.Fragment>
 	);
 };
