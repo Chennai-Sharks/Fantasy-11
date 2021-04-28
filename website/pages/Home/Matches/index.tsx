085e88dafef327508da80dc262a12adc56fdd9a6
Poujhit <poujhit.dev@gmail.com>
2021-04-28 18:27:01 +0530

    Added animations in match selection screen

--- website/pages/Home/Matches/index.tsx
+++ website/pages/Home/Matches/index.tsx
@@ -1,7 +1,16 @@
-import { AppBar, Button, Card, Toolbar, Typography } from '@material-ui/core';
+import {
+	AppBar,
+	Button,
+	Card,
+	StylesProvider,
+	Toolbar,
+	Typography,
+} from '@material-ui/core';
 import axios from 'axios';
 import React from 'react';
 import { useQuery } from 'react-query';
+import Image from 'next/image';
+
 import { MoonLoader } from 'react-spinners';
 import MatchCard from '../../../containers/Match/MatchCard';
 
@@ -9,13 +18,26 @@ import { useRouter } from 'next/router';
 
 import classes from '../../../styles/Match.module.scss';
 import versusMatchStore from '../../../stores/VersusMatchStore';
+import Head from 'next/head';
+import { motion, Variant } from 'framer-motion';
+
+const stagger = {
+	animate: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+};
+
 const MatchScreen: React.FC = () => {
 	const router = useRouter();
 
 	const { isLoading, isError, data, refetch } = useQuery(
 		'matches',
 		async () => {
-			const { data } = await axios.get('http://localhost:4000/api/match');
+			const { data } = await axios.get('http://localhost:4000/api/match', {
+				withCredentials: true,
+			});
 			return data;
 		},
 		{
@@ -28,45 +50,85 @@ const MatchScreen: React.FC = () => {
 	const matchStore = versusMatchStore((state) => state);
 
 	return (
-		<div className={classes.background}>
-			<Card raised className={classes.leftPortionCard}>
-				<AppBar position='static' style={{ backgroundColor: '#fd3a4b' }}>
-					<Toolbar>
-						<Typography variant='h6'>Welcome to Fantasy 11 League</Typography>
-					</Toolbar>
-				</AppBar>
-				<Typography className={classes.title}>Available Matches</Typography>
-				{isLoading ? (
-					<MoonLoader />
-				) : isError ? (
-					<Button onClick={() => refetch()}>Try Again</Button>
-				) : (
-					(data.teamInfo as Array<{
-						match: string;
-						teams: string[];
-					}>).map((eachMatchInfo, index) => {
-						return (
-							<MatchCard
-								key={index}
-								onTap={() => {
-									// console.log(eachMatchInfo['match'].substring(0, 6))
-									matchStore.setoneTeam(eachMatchInfo['teams'][0]);
-									matchStore.setTwoTeam(eachMatchInfo['teams'][1]);
-									router.push(
-										`matches/create-team/${eachMatchInfo['match'].substring(
-											0,
-											6
-										)}`
-									);
+		<motion.div
+			initial='initial'
+			animate='animate'
+			exit={{ opacity: 0 }}
+			className={classes.background}
+		>
+			<Head>
+				<title>Fantasy 11 | Matches</title>
+				<link rel='icon' href='/logo.png' />
+			</Head>
+			<StylesProvider injectFirst>
+				<Card raised className={classes.leftPortionCard}>
+					<AppBar position='static' style={{ backgroundColor: '#fd3a4b' }}>
+						<Toolbar
+							style={{
+								display: 'grid',
+								placeItems: 'center',
+							}}
+						>
+							<Typography
+								variant='h6'
+								align='center'
+								style={{
+									fontWeight: 'bold',
 								}}
-								one={eachMatchInfo['teams'][0]}
-								two={eachMatchInfo['teams'][1]}
-							/>
-						);
-					})
-				)}
-			</Card>
-		</div>
+							>
+								Welcome to Fantasy 11 League
+							</Typography>
+						</Toolbar>
+					</AppBar>
+					<Typography className={classes.title}>Available Matches</Typography>
+
+					{isLoading ? (
+						<MoonLoader />
+					) : isError ? (
+						<Button onClick={() => refetch()}>Try Again</Button>
+					) : (
+						(data.teamInfo as Array<{
+							match: string;
+							teams: string[];
+						}>).map((eachMatchInfo, index) => {
+							return (
+								<motion.div style={{ width: '100%' }} variants={stagger}>
+									<MatchCard
+										key={index}
+										onTap={() => {
+											matchStore.setoneTeam(eachMatchInfo['teams'][0]);
+											matchStore.setTwoTeam(eachMatchInfo['teams'][1]);
+											router.push(
+												`matches/create-team/${eachMatchInfo['match'].substring(
+													0,
+													6
+												)}`
+											);
+										}}
+										one={eachMatchInfo['teams'][0]}
+										two={eachMatchInfo['teams'][1]}
+									/>
+								</motion.div>
+							);
+						})
+					)}
+				</Card>
+			</StylesProvider>
+			<motion.div variants={stagger} className={classes.rightPortion}>
+				<div className={classes.logo} />
+				<Image
+					src='/logo.png'
+					alt='Logo of fantasy-11'
+					width={100}
+					height={100}
+					className={classes.logo}
+				/>
+				<Typography className={classes.title}>Fantasy 11</Typography>
+				<Typography className={classes.subTitle}>
+					Make sure you make the right choices
+				</Typography>
+			</motion.div>
+		</motion.div>
 	);
 };
 
