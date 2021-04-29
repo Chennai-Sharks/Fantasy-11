57c3af62387b53f0f84af5fbd63b1182994674de
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-29 19:53:31 +0530

    Fixed the facebook registration issue

--- server/models/User.js
+++ server/models/User.js
@@ -3,17 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   phone: {
     type: Number,
-    required: true,
     trim: true
   },
   email: {
     type: String,
-    required: true,
     lowercase: true
   },
   password: {
     type: String,
-    required: true,
     max: 1000,
     min: 8
   },
