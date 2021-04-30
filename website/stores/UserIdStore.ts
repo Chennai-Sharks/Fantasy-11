7cb5053fda8ea6714821ab5ba09f1ea7496691da
Poujhit <poujhit.dev@gmail.com>
2021-04-30 16:35:17 +0530

    Added store for userId to get points

---
+++ website/stores/UserIdStore.ts
@@ -0,0 +1,29 @@
+import create from 'zustand';
+
+import { persist } from 'zustand/middleware';
+
+type State = {
+	userId: string;
+
+	setuserId: (email: string) => void;
+};
+
+const userIdStore = create<State>(
+	persist(
+		(set) => ({
+			userId: '',
+
+			setuserId: (userId) =>
+				set((state) => ({
+					...state,
+					userId: userId,
+				})),
+		}),
+		{
+			name: 'userId',
+			getStorage: () => sessionStorage,
+		}
+	)
+);
+
+export default userIdStore;
