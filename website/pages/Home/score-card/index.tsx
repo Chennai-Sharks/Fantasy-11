704ec0b6b675b2e3b4b6cb9e5366d16e4eb6f05e
Poujhit <poujhit.dev@gmail.com>
2021-04-30 16:39:42 +0530

    Scoreboard 80 percent finished

--- website/pages/Home/score-card/index.tsx
+++ website/pages/Home/score-card/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 import classes from '@styles/CreateTeam.module.scss';
-import { Card, Typography } from '@material-ui/core';
+import { AppBar, Card, Toolbar, Typography } from '@material-ui/core';
+import LogoutButton from '@containers/Logout/LogoutButton';
 
 const Scorecard: React.FC = () => {
 	const router = useRouter();
@@ -20,9 +22,38 @@ const Scorecard: React.FC = () => {
 
 	return (
 		<div className={classes.background}>
-			<Card className={classes.leftPortionCard}></Card>
+			<Head>
+				<title>Fantasy 11 | Scoreboard</title>
+				<link rel='icon' href='/logo.png' />
+			</Head>
+			<Card className={classes.leftPortionCard}>
+				<AppBar
+					position='static'
+					style={{
+						backgroundColor: '#FD3A4A',
+					}}
+				>
+					<Toolbar
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+						}}
+					>
+						<Typography
+							variant='h6'
+							style={{ margin: '10px', textAlign: 'center' }}
+						>
+							ScoreBoard
+						</Typography>
+						<LogoutButton />
+					</Toolbar>
+				</AppBar>
+			</Card>
 			<div className={classes.rightPortion}>
 				<div className={classes.logo} />
+				<div className={classes.logo} />
+
 				<Image
 					src='/logo.png'
 					alt='Logo of fantasy-11'
