11d143be75afaf56d45bccbb62c8d85e18a099c1
Poujhit <poujhit.dev@gmail.com>
2021-04-30 12:13:26 +0530

    Added logout button functionality

--- website/containers/Logout/LogoutButton.tsx
+++ website/containers/Logout/LogoutButton.tsx
@@ -1,25 +1,52 @@
-import { Button } from '@material-ui/core';
+import { Button, Snackbar, Tooltip } from '@material-ui/core';
 import axios from 'axios';
 import React from 'react';
 import { useRouter } from 'next/router';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 interface LogoutButtonProps {}
 
-const LogoutButton: React.FC<LogoutButtonProps> = (props) => {
+const LogoutButton: React.FC<LogoutButtonProps> = () => {
 	const router = useRouter();
+	const [openAlert, setOpenAlert] = React.useState(false);
+	const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+
+		setOpenAlert(false);
+	};
 
 	return (
-		<Button
-			style={{}}
-			onClick={async () => {
-				await axios.get('http://localhost:4000/api/users/logout', {
-					withCredentials: true,
-				});
-				router.replace('/');
-			}}
-		>
-			logout
-		</Button>
+		<React.Fragment>
+			<Tooltip title='Logout' aria-label='logout'>
+				<Button
+					style={{
+						backgroundColor: '#fd3a4b',
+					}}
+					onClick={async () => {
+						setOpenAlert(true);
+						await axios.get('http://localhost:4000/api/users/logout', {
+							withCredentials: true,
+						});
+						router.replace('/');
+					}}
+				>
+					<ExitToAppIcon style={{ color: 'white' }} />
+				</Button>
+			</Tooltip>
+			<Snackbar
+				open={openAlert}
+				autoHideDuration={3000}
+				onClose={handleClose}
+				message={'Logging Out...'}
+				action={
+					<Button onClick={handleClose} style={{ color: 'white' }}>
+						Close
+					</Button>
+				}
+			/>
+		</React.Fragment>
 	);
 };
 export default LogoutButton;
