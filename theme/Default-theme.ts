9cdd1825d46dda9446c01b4adb81e44f28be415c
Poujhit <poujhit.dev@gmail.com>
2021-05-02 21:52:23 +0530

    Added default theme

---
+++ theme/Default-theme.ts
@@ -0,0 +1,26 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// You can create modify this theme or add new theme in any component. That theme will
+// override this default theme.
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#fd3a4b',
+		},
+		secondary: {
+			main: '#19857b',
+		},
+		error: {
+			main: red.A400,
+		},
+		background: {
+			default: '#ffffff',
+		},
+		text: {
+			secondary: '#ffffff',
+		},
+	},
+});
+
+export default theme;
