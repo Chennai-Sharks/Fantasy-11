8c54c06bff0fd5a85026cd5b20d045104098b88c
Poujhit <poujhit.dev@gmail.com>
2021-05-02 21:13:15 +0530

    Deleted website

--- website/next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-/// <reference types="next-images" />
-
-declare module '*.scss' {
-	const content: { readonly [className: string]: string };
-	export default content;
-}
