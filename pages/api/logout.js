1959f8e671cb9ba69837c6b5912af5f331489669
Poujhit <poujhit.dev@gmail.com>
2021-05-03 11:20:04 +0530

    Updated logout function

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,25 +2,33 @@ import cookie from 'cookie';
 import connectDB from '../../config/db';
 
 async function logout(req, res) {
-	res.setHeader('Set-Cookie', [
-		cookie.serialize('accessToken', '', {
-			httpOnly: true,
-			secure: false,
-			sameSite: 'strict',
-			path: '/',
-			maxAge: -1,
-		}),
-		cookie.serialize('authSession', '', {
-			httpOnly: false,
-			secure: false,
-			sameSite: 'strict',
-			path: '/',
-			maxAge: -1,
-		}),
-	]);
+	if (req.method !== 'POST')
+		return res
+			.status(405)
+			.json({ status: 'fail', message: 'Method not allowed here!' });
+	if (req.body.key === 'static_key') {
+		res.setHeader('Set-Cookie', [
+			cookie.serialize('accessToken', '', {
+				httpOnly: true,
+				secure: process.env.NODE_ENV !== 'development',
+				sameSite: 'strict',
+				path: '/',
+				maxAge: -1,
+			}),
+			cookie.serialize('authSession', '', {
+				httpOnly: false,
+				secure: process.env.NODE_ENV !== 'development',
+				sameSite: 'strict',
+				path: '/',
+				maxAge: -1,
+			}),
+		]);
 
-	// res.writeHead(302, { path: '/' });
-	res.send('done');
+		// res.writeHead(302, { path: '/' });
+		res.send('done');
+	} else {
+		req.status(404).send('not done');
+	}
 }
 
 export default connectDB(logout);
