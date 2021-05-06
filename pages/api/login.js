82794be9b68adf934ba854b07d1c15dd6ee499b4
Poujhit <poujhit.dev@gmail.com>
2021-05-06 14:18:34 +0530

    Updated dependencies

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,7 @@
+import cookie from 'cookie';
 import jwt from 'jsonwebtoken';
 import User from '../../models/User';
+
 import connectDB from '../../config/db';
 import saltHash from 'password-salt-and-hash';
 
@@ -22,6 +24,23 @@ async function login(req, res) {
 		},
 		process.env.JWT_AUTH_TOKEN
 	);
+
+	res.setHeader('Set-Cookie', [
+		cookie.serialize('accessToken', token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV !== 'development',
+			sameSite: 'strict',
+			path: '/',
+			expires: new Date(new Date().getTime() + 648000 * 1000),
+		}),
+		cookie.serialize('authSession', true, {
+			httpOnly: false,
+			secure: process.env.NODE_ENV !== 'development',
+			sameSite: 'strict',
+			path: '/',
+			expires: new Date(new Date().getTime() + 648000 * 1000),
+		}),
+	]);
 
 	res.json({
 		userId: user._id,
