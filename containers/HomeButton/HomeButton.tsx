8a087888c9cd0e11d97547da475557077a131285
Poujhit <poujhit.dev@gmail.com>
2021-05-06 22:33:38 +0530

    Added Home Button for easy Nav

---
+++ containers/HomeButton/HomeButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Tooltip, Button } from '@material-ui/core';
+import SportsCricketIcon from '@material-ui/icons/SportsCricket';
+interface HomeButtonProps {}
+
+const HomeButton: React.FC<HomeButtonProps> = () => {
+	const router = useRouter();
+	return (
+		<React.Fragment>
+			<Tooltip title='Home/Matches Page' aria-label='Home/Matches Page'>
+				<Button
+					style={{
+						backgroundColor: '#fd3a4b',
+					}}
+					onClick={() => {
+						router.push('/Home/Matches');
+					}}
+				>
+					<SportsCricketIcon style={{ color: 'white' }} />
+				</Button>
+			</Tooltip>
+		</React.Fragment>
+	);
+};
+export default HomeButton;
